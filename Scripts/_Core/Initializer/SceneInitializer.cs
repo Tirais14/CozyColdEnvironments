c8fda58233da7612fbf8f5306ad9686464b2096a
Tirais14 <100513231+Tirais14@users.noreply.github.com>
2025-07-20 01:33:29 +0300

    Clear

--- Scripts/_Core/Initializer/SceneInitializer.cs
+++ Scripts/_Core/Initializer/SceneInitializer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
-using UTIRLib.Attributes;
 using UTIRLib.Diagnostics;
 using UTIRLib.Init;
 using UTIRLib.Linq;
