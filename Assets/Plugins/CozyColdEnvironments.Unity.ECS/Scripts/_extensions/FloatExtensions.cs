08360128e7e4fd5a194cc1891ca7e3437e9e273c
Tirais14 <100513231+Tirais14@users.noreply.github.com>
2026-07-07 14:42:23 +0300

    Unity.ECS Update

---
+++ Assets/Plugins/CozyColdEnvironments.Unity.ECS/Scripts/_extensions/FloatExtensions.cs
@@ -0,0 +1,29 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+#nullable enable
+namespace CCEnvs.UnityX.ECS
+{
+    public static class FloatExtensions
+    {
+        public static Vector2 AsVector2(this in float2 source)
+        {
+            return new Vector2(source.x, source.y);
+        }
+
+        public static Vector3 AsVector3(this in float3 source)
+        {
+            return new Vector3(source.x, source.y, source.z);
+        }
+
+        public static Vector4 AsVector4(this in float4 source)
+        {
+            return new Vector4(source.x, source.y, source.z, source.w);
+        }
+
+        public static Vector2 ToVector2(this in float3 source)
+        {
+            return new Vector2(source.x, source.y);
+        }
+    }
+}
